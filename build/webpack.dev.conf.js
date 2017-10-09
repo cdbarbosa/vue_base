9a2ffd4e0d0bdb23240ee8ad6d3ee3624e5406ad
Eduardo Reis Nobre <eduardo.reisnobre@gmail.com>
2017-10-09 14:07:15 -0300

    dev fix

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -16,7 +16,7 @@ module.exports = merge(baseWebpackConfig, {
   },
   output: {
     publicPath: config.dev.assetsPublicPath 
-  }
+  },
   // cheap-module-eval-source-map is faster for development
   devtool: '#cheap-module-eval-source-map',
   plugins: [
