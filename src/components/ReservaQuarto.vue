c55bea6ca7871c4027f76b27f2e1d4e34cf8217f
Camila Dias <mylinhaa.94@gmail.com>
2018-01-15 16:17:20 -0300

    updates

--- src/components/ReservaQuarto.vue
+++ src/components/ReservaQuarto.vue
@@ -1,15 +1,37 @@
 <template lang="html">
-  <div class="">
+  <div id="reservaquarto">
     <div class="wrapper">
-      <h1 style="margin-top: 0; padding-top: 97px;">Cadastro</h1>
+    <div class="col col_area">
+      <div class="col col--1-of-2 col_cadastro">
+        <div class="login">
+          <h4>Já é cadastrado?</h4>
+          <label for="E-MAIL">E-MAIL</label>
+          <input type="text" name="" v-model="email" v-mask="'#######@#######.com'" autocomplete="on" placeholder="example@gmail.com" class="input">
+          <label for="SENHA">SENHA</label>
+          <input type="text" name="" v-model="senha" v-mask="'#########'" autocomplete="on" placeholder="123456789" class="input">
+          <button type="button" name="button">LOGIN</button>
+        </div>
+      </div>
+      <div class="col col--1-of-2 col_login">
+        <div class="cadastro">
+          <h4>Não é cadastrado?</h4>
+          <h5>Cadastre-se para fazer a reserva</h5>
+          <button type="button" name="button">CADASTRAR</button>
+        </div>
+      </div>
+    </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'reservaquarto',
+  data () {
+    return {
+      email: '',
+      senha: ''
+    }
+  }
 }
 </script>
-
-<style lang="css">
-</style>
