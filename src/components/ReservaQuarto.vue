d476248ad63414a2703f55e8932d5678c14da1b6
Eduardo Reis Nobre <eduardo.reisnobre@gmail.com>
2018-01-17 16:40:39 -0300

    update

--- src/components/ReservaQuarto.vue
+++ src/components/ReservaQuarto.vue
@@ -1,26 +1,16 @@
 <template lang="html">
-  <div id="reservaquarto">
+  <div id="reservaquarto" class="page">
     <div class="wrapper">
-      <div class="col col_area">
-        <div class="col col--1-of-2 col_cadastro">
-          <div class="login">
-            <h4>Já é cadastrado?</h4>
-            <label for="E-MAIL">E-MAIL</label>
-            <input type="text" name="" v-model="email" v-mask="'#######@#######.com'" autocomplete="on" placeholder="example@gmail.com" class="input">
-            <label for="SENHA">SENHA</label>
-            <input type="text" name="" v-model="senha" v-mask="'#########'" autocomplete="on" placeholder="123456789" class="input">
-            <router-link :to="{name: 'pagamento'}">
-              <button type="button" name="button">LOGIN</button>
-            </router-link>
-          </div>
-        </div>
-        <div class="col col--1-of-2 col_login">
-          <div class="cadastro">
-            <h4>Não é cadastrado?</h4>
-            <h5>Cadastre-se para fazer a reserva</h5>
-            <button type="button" name="button">CADASTRAR</button>
-          </div>
-        </div>
+      <div class="login">
+        <label for="E-MAIL">E-MAIL</label>
+        <input type="text" name="" v-model="email" autocomplete="on" placeholder="example@gmail.com" class="input">
+        <label for="SENHA">SENHA</label>
+        <input type="text" name="" v-model="senha" autocomplete="on" placeholder="123456789" class="input">
+        <router-link :to="{name: 'pagamento'}" tag="button" class="__small">
+          LOGIN
+          </router-link>
+          <span>Esqueceu a senha?</span> <span>Cadastre-se</span>
+      </div>
       </div>
     </div>
   </div>
